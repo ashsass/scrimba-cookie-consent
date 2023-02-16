956786cd6a04595eb52276537af0297d7a1c838c
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-02-16 11:51:03 -0500

    Added modal and input fields

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const modal = document.getElementById('modal')
+const modalClose = document.getElementById('modal-close-btn')
 
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
+
+modalClose.addEventListener('click', function() {
+    modal.style.display = 'none'
+})
