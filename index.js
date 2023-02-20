b99f51f699cd97d1df1f4c66f514f0aaee7d8068
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-02-20 11:26:56 -0500

    Finished the setTimeout transitions for the consent form

--- index.js
+++ index.js
@@ -14,10 +14,37 @@ modalClose.addEventListener('click', function() {
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
     modalText.innerHTML = 
-        '<div class="modal-inner-loading">' +
-            '<img src="images/loading.svg" class="loading">' +
-            '<p id="uploadText">' +
-                'Uploading your data to the dark web...' +
-            '</p>' +
-        '</div>'
+        `<div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="uploadText">
+                Uploading your data to the dark web...
+            </p>
+        </div>`
+
+    setTimeout(function(){
+        document.getElementById('uploadText').innerText = `Making the sale. . .`
+    }, 1500)
+
+    setTimeout(function() {
+        const modalInner = document.getElementById("modal-inner")
+        const consentFormData = new FormData(consentForm)
+        const formName = consentFormData.get('modal-name')
+    
+        modalInner.innerHTML = 
+            `<h2>Thanks <span class='modal-display-name'>${formName}</span>, you sucker! </h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="images/pirate.gif">
+            </div>`
+        console.log(consentFormData)
+    }, 3000)
 })
+/*   
+Challenge: 
+1. Create a const to store the user's name and
+   use a FormData method to extract the 
+   submitted name from the FormData object.
+2. Insert the user's name into the HTML string
+   that contains the final message we show our
+   users.
+*/ 
